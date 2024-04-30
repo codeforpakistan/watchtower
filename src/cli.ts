419f36ae2d250331c80d335b4c3df652d64cca43
Humble Penguin <humblepenguinn@gmail.com>
2024-04-30 13:33:51 +0500

    Setup basic project layout
    This commit sets up the basic layout for the program. It transitions
    from NodeJS to Bun, a more efficient alternative that promises enhanced
    performance which is something very critical for the tool's
    requirements.
    
    Additionally I have integrated the yargs library to introduce a
    rudimentary command line interface for the tool. The CLI currently
    supports a singular command named `generate <filename>`, designed to
    recieve a filename parameter listing the websites we want to generate a
    report for. Here is a example of the contents of the file:
    
    ```
    https://www.amazon.com/
    https://www.google.com/
    ```
    
    Upon execution, the crawler component of the program reads the filename,
    and creates a folder for each website inside the `reports` folder. The
    generated reports are then saved within their respective folders.

---
+++ src/cli.ts
@@ -0,0 +1,45 @@
+import fs from "fs";
+import readline from "readline";
+
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+import { generateReport } from "./crawler";
+
+yargs(hideBin(process.argv))
+  .command(
+    "generate <filename>",
+    `Generate a report on the list of web pages in the specified file`,
+    (yargs) =>
+      yargs.positional("filename", {
+        description:
+          "The name of the file containing the list of web pages to generate a report on",
+        type: "string",
+      }),
+    (argv: any) => {
+      const filename = argv.filename;
+      
+      if (!fs.existsSync(filename)) {
+        throw new Error("File does not exist");
+      }
+
+      const fileStream = fs.createReadStream(filename);
+
+      const rl = readline.createInterface({
+        input: fileStream,
+        crlfDelay: Infinity,
+      });
+
+      let lines: string[] = [];
+
+      rl.on("line", (line) => {
+        lines.push(line);
+      });
+
+      rl.on("close", () => {
+        for (let line of lines) {
+          generateReport(line);
+        }
+      });
+    }
+  )
+  .parse();
